1c9902a463a232584c540a60789fd301fdb944c5
srivatsamc <srivatsamcgm@gmail.com>
2021-06-24 17:39:13 +0530

    update

---
+++ BodyMassIndex.cs/Calculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BodyMassIndex.cs
+{
+    class Calculator
+    {
+        private int height, weight1;
+
+        public void setHeight(int H)
+        {
+            this.height = H;
+          
+        }
+
+            public void setWeight(int W)
+        {
+            this.weight1 = W;
+        }
+
+        public int bodyMass()
+        {
+            return (weight / height);
+        }
+    }
+}
---
+++ BodyMassIndex.cs/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BodyMassIndex.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+#region Taking inputs from User
+            System.Console.WriteLine("Enter your Height in meters :");
+
+            int H = Convert.ToInt32(Console.ReadLine());
+
+            System.Console.WriteLine("Enter your Weight in Kilograms :");
+
+            float W = Convert.ToInt32(Console.ReadLine());
+            
+            
+            #endregion
+#region Loading value of height and weight
+            calculator.setHeight(H);
+            calculator.setweight1(W);
+
+            #endregion
+
+            int BodyMass=calculator.bodyMass();
+
+            if (BodyMass < 18.5)
+            {
+                Console.WriteLine("You are UNDERWEIGHT");
+            }
+            else if (BodyMass >= 25)
+            {
+                Console.WriteLine("You are Healthy");
+            }
+            else
+            {
+                Console.WriteLine("You are between average");
+            }
+        }
+    }
+}
