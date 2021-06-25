4a5d01d316bff94eecfa008a79ddc005cb67790c
Venugopal K <venu.shastri@gmail.com>
2021-06-25 16:02:26 +0530

    Update PatientDetails.cs

--- BMICalculationApp/PatientDetails.cs
+++ BMICalculationApp/PatientDetails.cs
@@ -8,40 +8,16 @@ namespace BMICalculationApp
 {
      class PatientDetails
     {
-        BMICalculator calculation = new BMICalculator();
-        BMIValidator validate = new BMIValidator();
+       
 #region UDT
 
         private float heightInMeters;
         private float weightInKilos;
-        
+        private float bmiValue; 
 
         #endregion
 
-        public void ReadDetails()
-        {
-#region Reading Details of Patient
-            Console.WriteLine("Enter your Height in Meters");
-            this.heightInMeters = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter your Weight in Kilos");
-            this.weightInKilos = Convert.ToInt32(Console.ReadLine());
-            #endregion
-#region BMI Calculation
-            float bmiValue= calculation.CalculateBMI( this.weightInKilos,this.heightInMeters);
-            #endregion
-#region BMI value Validation
-            validate.CheckCondition(bmiValue);
-#endregion
+        
         }
-     
-      
-
-
-       
-        
-        
-      
-        
     
-    }
 }
