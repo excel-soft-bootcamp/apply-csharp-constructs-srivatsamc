9a388b930464bbf6af3d4fb1bc0d5b3f32c6d71b
srivatsamc <78139019+srivatsamc@users.noreply.github.com>
2021-06-25 12:41:27 +0530

    Add files via upload

---
+++ BMICalculationApp/BMIValidator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMICalculationApp
+{
+    public class BMIValidator
+    {
+        Display consoledisplay = new Display();
+#region Checking BMIvalue 
+        public void CheckCondition(float BMIValue)
+        {
+            if (BMIValue < 18.5)
+            {
+                consoledisplay.Message1();
+            }
+            else if (BMIValue >= 25)
+            {
+                consoledisplay.Message2();
+            }
+            else
+            {
+                consoledisplay.Message3();
+            }
+        }
+#endregion
+    }
+}
---
+++ BMICalculationApp/Display.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMICalculationApp
+{
+   public class Display
+    {
+ #region Message display
+        public void Message1()
+        {
+            
+           Console.WriteLine("You are UNDERWEIGHT");
+        }
+        public void Message2()
+        {
+            Console.WriteLine("You are OVERWEIGHT");
+        }
+        public void Message3()
+        {
+            Console.WriteLine("You are HEALTHY");
+        }
+    
+    #endregion
+
+}
+}
