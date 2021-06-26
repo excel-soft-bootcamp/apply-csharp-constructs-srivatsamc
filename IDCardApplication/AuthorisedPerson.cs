1a4066ddea6aa76bb76c163d3dfc3130bf967cb9
srivatsamc <srivatsamcgm@gmail.com>
2021-06-26 12:56:35 +0530

    Update

---
+++ IDCardApplication/AuthorisedPerson.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDCardApplication
+{
+    public abstract class AuthorisedPerson
+    {
+        #region UDT
+
+        private string _name,_department,_contact_no,_id_no;
+
+        #endregion
+
+        #region Overloaded Constructors of Abstract Class AuthorisedPerson
+
+        public AuthorisedPerson(string name)
+        {
+            this._name = name;
+        }
+
+        public AuthorisedPerson(string name,string department)
+        {
+            this._name = name;
+            this._department = department;
+        }
+
+        public AuthorisedPerson(string name, string department,string contact_no)
+        {
+            this._name = name;
+            this._department = department;
+            this._contact_no = contact_no;
+        }
+
+        public AuthorisedPerson(string name, string department, string contact_no,string id_no)
+        {
+            this._name = name;
+            this._department = department;
+            this._contact_no = contact_no;
+            this._id_no = id_no;
+        }
+
+        #endregion
+    }
+}
---
+++ IDCardApplication/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDCardApplication
+{
+    public class Student:AuthorisedPerson
+    {
+        #region UDT
+
+        private string _usn;
+
+        #endregion
+
+        #region Method declaration
+        public void AttendClass() { }
+
+        #endregion
+
+        #region Overloaded Constructor of Child Class Student
+
+        public Student(string usn, string name, string department, string contact_no, string id_no) : base(name, department, contact_no, id_no)
+        {
+            this._usn = usn;
+        }
+
+        #endregion
+
+    }
+}
