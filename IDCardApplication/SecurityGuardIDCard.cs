f349f298b309271e137581d9501bcdb5c7849cbd
srivatsamc <srivatsamcgm@gmail.com>
2021-06-27 09:08:15 +0530

    Update

---
+++ IDCardApplication/SecurityGuardIDCard.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDCardApplication
+{
+    public class SecurityGuardIDCard:IDCard
+    {
+
+        private string _timings;
+
+        #region Overloaded Constructor of Child Class SecurityGuard
+
+        public SecurityGuardIDCard(string timings, string name, string id_no) :base (name, id_no)
+        {
+            this._timings = timings;
+        }
+
+        public SecurityGuardIDCard(string timings, string name, string id_no,string contact_no) : base(name, id_no,contact_no)
+        {
+            this._timings = timings;
+        }
+
+        public SecurityGuardIDCard(string timings, string name, string id_no, string contact_no,string bloodgroup) : base(name, id_no, contact_no,bloodgroup)
+        {
+            this._timings = timings;
+        }
+        #endregion
+    }
+}
